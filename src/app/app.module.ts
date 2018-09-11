89d85ff04f12db201d73fa01e8163ee36331a474
Marcus Henning <marcus.henning4@gmail.com>
2018-09-11 19:24:24 +0200

    Material Componenten importiert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatStepperModule, MatCardModule, MatTooltipModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddUserFormComponent } from './add-user-form/add-user-form.component';
 
 import { AppRoutingModule } from './app-routing.module'
 import { MemberListModule } from './member-list/member-list.module';
@@ -9,13 +13,31 @@ import { MemberViewModule } from './member-view/member-view.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserFormComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     MemberListModule,
     MemberViewModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    AddUserFormComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
