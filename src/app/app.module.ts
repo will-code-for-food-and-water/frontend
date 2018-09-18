cdff0bf275ad1ee776ddab99d0ec6f642cb23202
unknown <marcus.henning4@gmail.com>
2018-09-18 09:44:20 +0200

    Validierung eingefügt, noch nicht vollständig
    Signed-off-by: unknown <marcus.henning4@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatStepperModule, MatCardModule, MatTooltipModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, PatternValidator } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AddUserFormComponent } from './add-user-form/add-user-form.component';
+import { NumberOnlyDirective } from './number.directive';
 
 import { AppRoutingModule } from './app-routing.module'
 import { MemberListModule } from './member-list/member-list.module';
@@ -14,7 +15,8 @@ import { MemberViewModule } from './member-view/member-view.module';
 @NgModule({
   declarations: [
     AppComponent,
-    AddUserFormComponent
+    AddUserFormComponent,
+    NumberOnlyDirective
   ],
   imports: [
     AppRoutingModule,
