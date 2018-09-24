95749700180fa17904307df090f2792fd3387917
Thomas <thomasbirke@gmx.de>
2018-09-24 18:28:30 +0200

    01.003F: Clean app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatStepperModule, MatCardModule, MatTooltipModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { AddUserFormComponent } from './add-user-form/add-user-form.component';
-import { NumberOnlyDirective } from './number.directive';
 
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
 import { MemberListModule } from './member-list/member-list.module';
 import { MemberViewModule } from './member-view/member-view.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddUserFormComponent,
-    NumberOnlyDirective
+    AppComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MemberListModule,
     MemberViewModule
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatSelectModule,
-    MatStepperModule,
-    MatCardModule,
-    MatTooltipModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
   ],
   entryComponents: [
     AddUserFormComponent
   ],
-  providers: [ { provide: LOCALE_ID, useValue: 'ger' } ],
+  providers: [{ provide: localeDe, useValue: 'de' }],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
- }
+export class AppModule {
+}
