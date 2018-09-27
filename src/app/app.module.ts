75fa32bad2e4752cd7b47af3801dda0ee92f2459
Unknown <ykrissel@googlemail.com>
2018-09-27 17:35:39 +0200

    - rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { UserComponent } from './user/user.component';
-import {AppRoutingModule} from './core/app.routing.module';
 import { LoginComponent } from './login/login.component';
 import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {RouterModule} from '@angular/router';
-import {MaterialModule} from './core/material.module';
-
-
-
 
 import { AppRoutingModule } from './app-routing.module'
 import { MemberListModule } from './member-list/member-list.module';
