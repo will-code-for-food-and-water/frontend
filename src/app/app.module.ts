7c76c1c8d94850d5185832afda922eb2bcfd44e6
JT <j.toengi@gmail.com>
2018-10-07 17:34:05 +0200

    03.002F Rebase to master finalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material';
 import { AppComponent } from './app.component';
-import { AddUserFormComponent } from './add-user-form/add-user-form.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MemberListModule } from './member-list/member-list.module';
@@ -12,24 +11,21 @@ import { PlaneListModule } from './plane-list/plane-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
     MemberListModule,
     MemberViewModule,
-    PlaneListModule,
-    AddPlaneFormModule
+    PlaneListModule
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'ger' }
   ],
+  bootstrap: [AppComponent],
   entryComponents: [
     EditMemberDialogComponent
   ]
-  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule { }
